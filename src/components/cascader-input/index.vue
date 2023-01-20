e9aa8a137fb2a0a05bb6db028713e635778e83f4
shinetingli <shinetingli@tencent.com>
2023-01-20 18:26:19 +0800

    feat: 新增编辑表单

---
+++ src/components/cascader-input/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <t-select
+      v-model="type"
+      :options="options"
+      placeholder="请选择值类型"
+      @change="handleSelectType"
+    ></t-select>
+    <input-field v-if="type" :type="type"></input-field>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Select as TSelect, type SelectValue } from "tdesign-vue-next";
+import { ref } from "vue";
+import { cascaderSelectOptions } from "@/utils/config";
+import InputField from '../input-field/index.vue';
+
+const options = ref(cascaderSelectOptions);
+const type = ref("");
+
+const handleSelectType = (value: SelectValue) => {
+  // switch(value) {
+  //   case 'Number'
+  // }
+}
+</script>
---
+++ src/utils/config.ts
@@ -0,0 +1,6 @@
+export const cascaderSelectOptions = [
+  { label: "字符串", value: "String" },
+  { label: "数值", value: "Number" },
+  { label: "布尔值", value: "Boolean" },
+  { label: "随机值", value: "Random" },
+];
