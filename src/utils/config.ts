7bd037a379189dfec4eb5ea4b1cebe7bf67da27e
shinetingli <shinetingli@tencent.com>
2023-01-23 11:22:29 +0800

    feat: 新增自定义节点模式

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,5 +2,5 @@ export const cascaderSelectOptions = [
   { label: "字符串", value: "String" },
   { label: "数值", value: "Number" },
   { label: "布尔值", value: "Boolean" },
-  { label: "随机值", value: "Random" },
+  { label: "自定义", value: "Custom" },
 ];
