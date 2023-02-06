837d289f8216b1b0445c05ef5789ef8e628bb145
shinetingli <shinetingli@tencent.com>
2023-02-06 20:22:05 +0800

    fix: 完善发布配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,28 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import svgLoader from "vite-svg-loader";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
+import commonjs from 'rollup-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), svgLoader(), dts()],
+  plugins: [ vue(), vueJsx(), svgLoader(), dts()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./packages", import.meta.url)),
     },
   },
   build: {
+    target: "modules",
     lib: {
       entry: resolve(__dirname, "packages/index.ts"),
-      formats: ["es"],
+      formats: ["es", "umd"],
       fileName: "index",
+      name: 'index',
     },
     copyPublicDir: false,
     rollupOptions: {
       external: ["vue", "tdesign-vue-next", "tdesign-icons-vue-next"],
+      plugins: [commonjs()],
     },
   },
 });
